8adcc178ea85657d0a79e2d77594f6a1b8bb1197
Victor Levy <victorlevygp@gmail.com>
2020-07-03 11:21:57 -0300

    loading api script in promise. Installed dotenv and stored MAPS_API_KEY as global variable

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-import initMap from './scripts/initMap';
+import initMap from "./scripts/initMap";
 
-import '../src/style/style.scss';
+import "../src/style/style.scss";
 
 
-initMap();
+const addMapsApiScript = new Promise((resolve, reject) => {
+  let script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.MAPS_API_KEY}&libraries=places`;
+  script.addEventListener("load", () => resolve(script), false);
+  script.addEventListener("error", () => reject(script), false);
+  document.body.appendChild(script);
+});
+
+const renderScriptLoadError = () => {
+  const errorMessage = document.createElement('span');
+  errorMessage.classList.add('api-load-error');
+  errorMessage.innerHTML = 'Erro ao carregar o mapa';
+  document.querySelector('#map').appendChild(errorMessage);
+}
+
+addMapsApiScript.then(() => initMap()).catch(renderScriptLoadError());
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,40 @@
-const path = require('path');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+const dotenv = require("dotenv").config();
 
 module.exports = {
+  node: { fs: "empty" },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
         },
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: 'html-loader',
-          }
+            loader: "html-loader",
+          },
         ],
       },
       {
-        test: /\.s(a|c)ss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: './public/index.html',
-      filename: './index.html',
-    })
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+    new webpack.DefinePlugin({
+      "process.env": JSON.stringify(dotenv.parsed),
+    }),
   ],
 };
